4dbf6c44515bb98464dad1acd25a882b597e074f
POPCORN IT <108529863+popcornbd@users.noreply.github.com>
2022-10-31 18:36:41 +0600

    'form'

--- components/Domain.jsx
+++ components/Domain.jsx
@@ -1,5 +1,6 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import {
+  Alert,
   Button,
   Grid,
   IconButton,
@@ -7,7 +8,7 @@ import {
   Link,
   TextField,
   Typography,
-  useTheme,
+  useTheme, Snackbar,
 } from "@mui/material";
 import { Box, Container } from "@mui/system";
 import PersonIcon from "@mui/icons-material/Person";
@@ -18,46 +19,85 @@ import AttachMoneySharpIcon from "@mui/icons-material/AttachMoneySharp";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
 import { ColorModeContext } from "../pages";
-
+import emailjs from "@emailjs/browser";
+import { Close } from '@mui/icons-material'
 export default function Domain() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [offer, setOffer] = useState("");
+  const [open, setOpen] = useState(false);
+  const form = useRef();
   const theme = useTheme();
   const colorMode = useContext(ColorModeContext);
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
   const handleSubmit = (event) => {
-    console.log("Name: ", name);
-    if(name === '' || email === '' || offer === '') {
-      alert('Please fill all the fields')
-      return
-    }else{
-    event.preventDefault();
+    if (name === "" || email === "" || offer === "") {
+      alert("Please fill all the fields");
+    }
+    else{event.preventDefault();
     setName("");
     setEmail("");
     setOffer("");
-    window.location = 'mailto:sales@popcorn.com.bd?subject=Domain Offer&body=Name: '+name+'%0D%0AEmail: '+email+'%0D%0AOffer: '+offer
-    }
+    setOpen(true);
+    emailjs.sendForm(
+          "service_l2jfd8d",
+          "template_zr3h6l8",
+          form.current,
+          "TaTGlAfHC6tC03lO_"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+            console.log('success')
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );}
   };
   return (
     <div style={{}}>
-      <Box sx={{bgcolor:'background.default', color:'text.primary', height:{lg:'93vh', xs:'100%'}}}>
+      <Box
+        sx={{
+          bgcolor: "background.default",
+          color: "text.primary",
+          height: { lg: "93vh", xs: "100%" },
+        }}
+      >
         <IconButton onClick={colorMode.toggleColorMode} color="inherit">
           {theme.palette.mode === "dark" ? (
-            <Brightness7Icon sx={{color:'text.primary'}}/>
+            <Brightness7Icon sx={{ color: "text.primary" }} />
           ) : (
             <Brightness4Icon />
           )}
         </IconButton>
-        <Container sx={{height:'100%', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
-          <Grid container sx={{alignItems:'center'}}>
-            <Grid item lg={8} xs={12} >
+        <Container
+          sx={{
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Grid container sx={{ alignItems: "center" }}>
+            <Grid item lg={8} xs={12}>
               <Box>
                 <Typography
                   variant="h3"
                   sx={{
-                    bgcolor: `${theme.palette.mode === "dark" ? "#ffa726" : "#d32f2f"}`,
+                    bgcolor: `${
+                      theme.palette.mode === "dark" ? "#ffa726" : "#d32f2f"
+                    }`,
                     display: "inline",
-                    color: `${theme.palette.mode === "dark" ? "#212121" : "#fff"}`,
+                    color: `${
+                      theme.palette.mode === "dark" ? "#212121" : "#fff"
+                    }`,
                     p: "0.2em 0.5em",
                     fontWeight: "bold",
                     borderRadius: "3px",
@@ -67,11 +107,18 @@ export default function Domain() {
                 </Typography>
                 <Typography
                   variant="h3"
-                  sx={{ fontWeight: "bold", my: "0.5em", color:'text.primary' }}
+                  sx={{
+                    fontWeight: "bold",
+                    my: "0.5em",
+                    color: "text.primary",
+                  }}
                 >
                   MyDomain.com
                 </Typography>
-                <Typography variant="h5" sx={{ display: "inline", color:'text.primary'}}>
+                <Typography
+                  variant="h5"
+                  sx={{ display: "inline", color: "text.primary" }}
+                >
                   Estimated value
                 </Typography>
                 <Typography
@@ -88,7 +135,10 @@ export default function Domain() {
                 >
                   $650
                 </Typography>
-                <Typography variant="body1" sx={{ my: "1em", color:'text.primary' }}>
+                <Typography
+                  variant="body1"
+                  sx={{ my: "1em", color: "text.primary" }}
+                >
                   12 years old premium domain name available for instantly
                   punches. Invest in a premium domain name to launch your brand{" "}
                 </Typography>
@@ -96,53 +146,96 @@ export default function Domain() {
                   sx={{
                     display: "flex",
                     justifyContent: "space-between",
-                    width: {lg:"80%", xs:'100%'},
+                    width: { lg: "80%", xs: "100%" },
                     flexWrap: "wrap",
-                    gap: {lg:"0", xs:'1em'},
+                    gap: { lg: "0", xs: "1em" },
                   }}
                 >
                   <Box sx={{ display: "flex", alignItems: "center" }}>
-                    <CallIcon sx={{color:'text.primary'}}/>
-                    <Typography variant="body1" sx={{ display: "inline",color:'text.primary' }}>
-                      <Link href='tel:(+91) 11-2142-4568' underline='none' color="text.light">(+91) 11-2142-4568</Link>
+                    <CallIcon sx={{ color: "text.primary" }} />
+                    <Typography
+                      variant="body1"
+                      sx={{ display: "inline", color: "text.primary" }}
+                    >
+                      <Link
+                        href="tel:(+91) 11-2142-4568"
+                        underline="none"
+                        color="text.light"
+                      >
+                        (+91) 11-2142-4568
+                      </Link>
                     </Typography>
                   </Box>
                   <Box sx={{ display: "flex", alignItems: "center" }}>
-                    <MailIcon sx={{color:'text.primary'}}/>
-                    <Typography variant="body1" sx={{ display: "inline",color:'text.primary' }}>
-                      <Link href="mailto:info@mydomain.com" underline='none' color="text.light">info@mydomain.com</Link>
+                    <MailIcon sx={{ color: "text.primary" }} />
+                    <Typography
+                      variant="body1"
+                      sx={{ display: "inline", color: "text.primary" }}
+                    >
+                      <Link
+                        href="mailto:info@mydomain.com"
+                        underline="none"
+                        color="text.light"
+                      >
+                        info@mydomain.com
+                      </Link>
                     </Typography>
                   </Box>
                   <Box sx={{ display: "flex", alignItems: "center" }}>
-                    <LanguageIcon sx={{color:'text.primary'}}/>
-                    <Typography variant="body1" sx={{ display: "inline",color:'text.primary' }}>
-                      <Link underline='none' color="text.light">More Domain</Link>
+                    <LanguageIcon sx={{ color: "text.primary" }} />
+                    <Typography
+                      variant="body1"
+                      sx={{ display: "inline", color: "text.primary" }}
+                    >
+                      <Link underline="none" color="text.light" href="#">
+                        More Domain
+                      </Link>
                     </Typography>
                   </Box>
                 </Box>
               </Box>
             </Grid>
             <Grid item lg={4} xs={12}>
-              <Box sx={{ border: `${theme.palette.mode === 'dark' ? 'none' : "1px solid lightgray"}`, p: "1em 1.2em", bgcolor:`${theme.palette.mode === 'dark' ? '#55536e3b' : '#fff'}`, my:{lg:'0', xs:'3em'},height:'450px', display:'flex', flexDirection:'column', justifyContent:'center', boxShadow:'rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px' }}>
+              <Box
+                sx={{
+                  border: `${
+                    theme.palette.mode === "dark"
+                      ? "none"
+                      : "1px solid lightgray"
+                  }`,
+                  p: "1em 1.2em",
+                  bgcolor: `${
+                    theme.palette.mode === "dark" ? "#55536e3b" : "#F8F9FA"
+                  }`,
+                  my: { lg: "0", xs: "3em" },
+                  height: "450px",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  boxShadow: "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
+                }}
+              >
                 <Box sx={{ textAlign: "center", mb: "1.5em" }}>
-                  <Typography variant="h6" sx={{ fontWeight: "bold",color:'text.primary' }}>
+                  <Typography
+                    variant="h6"
+                    sx={{ fontWeight: "bold", color: "text.primary" }}
+                  >
                     Make Your Offer
                   </Typography>
                   <Typography
                     variant="body2"
-                    sx={{ color:'text.primary', my: "1em" }}
+                    sx={{ color: "text.primary", my: "1em" }}
                   >
                     Please fill out the form below so that the seller receive
                     your offer
                   </Typography>
                 </Box>
-                <Box
-                  sx={{ gap: "1em", display: "flex", flexDirection: "column" }}
-                >
+                  <form ref={form} onSubmit={handleSubmit} style={{gap: "1em", display: "flex", flexDirection: "column" }}>
                   <TextField
                     fullWidth
                     size="large"
                     placeholder="Enter Your Name"
+                    type='text'
                     onChange={(e) => setName(e.target.value)}
                     value={name}
                     InputProps={{
@@ -157,6 +250,7 @@ export default function Domain() {
                     fullWidth
                     placeholder="Enter Your Email"
                     size="large"
+                    type='email'
                     onChange={(e) => setEmail(e.target.value)}
                     value={email}
                     InputProps={{
@@ -171,6 +265,7 @@ export default function Domain() {
                     fullWidth
                     placeholder="Enter Your Offer"
                     size="large"
+                    type="number"
                     onChange={(e) => setOffer(e.target.value)}
                     value={offer}
                     InputProps={{
@@ -185,13 +280,32 @@ export default function Domain() {
                     variant="contained"
                     fullWidth
                     size="large"
-                    sx={{ bgcolor: `${theme.palette.mode === "dark" ? "#ffa726" : null}`, textTransform:'capitalize' }}
+                    type="submit"
+                    sx={{
+                      bgcolor: `${
+                        theme.palette.mode === "dark" ? "#ffa726" : null
+                      }`,
+                      textTransform: "capitalize",
+                    }}
                     onClick={handleSubmit}
                   >
                     Send my offer
                   </Button>
+                  </form>
+                  <Snackbar
+                    open={open}
+                    autoHideDuration={3500}
+                    onClose={handleClose}
+                  >
+                    <Alert
+                      onClose={handleClose}
+                      severity="success"
+                      sx={{ width: "100%" }}
+                    >
+                      Successfully Send Your Offer
+                    </Alert>
+                  </Snackbar>
                 </Box>
-              </Box>
             </Grid>
           </Grid>
         </Container>
